68f48fe0b620d0f8f62d1657351645d62fb9b32a
PRASHANTKUMAR_7 <prashantkumar74806@gmail.com>
2024-07-09 13:29:01 +0530

    adding html code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import hotbg from "./assets/hot.jpg";
+import coldbg from "./assets/cold.jpg";
+import hazebg from "./assets/haze.jpg";
+import rainbg from "./assets/rainy.jpeg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{backgroundImage: `url(${coldbg})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section--inputs">
+            <input type="text" name="city" placeholder="Enter City Name..."/>
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>varanasi</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png"alt="weather" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

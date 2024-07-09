7dc2dfcea2c4d1b2aae184887750f070956d2473
PRASHANTKUMAR_7 <prashantkumar74806@gmail.com>
2024-07-10 01:06:24 +0530

    adding css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import hotbg from "./assets/hot.jpg";
 import coldbg from "./assets/cold.jpg";
 import hazebg from "./assets/haze.jpg";
 import rainbg from "./assets/rainy.jpeg";
+import Description from "./components/Description";
 function App() {
   return (
     <div className="app" style={{backgroundImage: `url(${coldbg})`}}>
       <div className="overlay">
         <div className="container">
-          <div className="section section--inputs">
+          <div className="section section__inputs">
             <input type="text" name="city" placeholder="Enter City Name..."/>
             <button>°F</button>
           </div>
           <div className="section section__temperature">
             <div className="icon">
-              <h3>varanasi</h3>
-              <img src="https://openweathermap.org/img/wn/02d@2x.png"alt="weather" />
+              <h3>Varanasi,UP</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png"alt="weather-icon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
             </div>
           </div>
+          {/* bottom description */}
+          <Description/>
         </div>
       </div>
     </div>
---
+++ src/components/Description.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaArrowDown } from "react-icons/fa";
+
+const Description = () => {
+  return (
+    <div className="section section__description">
+        <div className="card">
+            <div className="description__card-icon">
+            <FaArrowDown />
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Description

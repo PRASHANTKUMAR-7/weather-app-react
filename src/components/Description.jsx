cc3215ceb2e96a0d117005a36b032f87b536c6d4
PRASHANTKUMAR_7 <prashantkumar74806@gmail.com>
2024-07-10 01:26:49 +0530

    adding css

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import './Description.css';
+
 import { FaArrowDown } from "react-icons/fa";
 
 const Description = () => {
@@ -7,10 +9,48 @@ const Description = () => {
         <div className="card">
             <div className="description__card-icon">
             <FaArrowDown />
+            <small>min</small>
             </div>
+            <h2>32 °C</h2>
+        </div>
+        <div className="card">
+            <div className="description__card-icon">
+            <FaArrowDown />
+            <small>min</small>
+            </div>
+            <h2>32 °C</h2>
+        </div>
+        <div className="card">
+            <div className="description__card-icon">
+            <FaArrowDown />
+            <small>min</small>
+            </div>
+            <h2>32 °C</h2>
+        </div>
+        <div className="card">
+            <div className="description__card-icon">
+            <FaArrowDown />
+            <small>min</small>
+            </div>
+            <h2>32 °C</h2>
+        </div>
+        <div className="card">
+            <div className="description__card-icon">
+            <FaArrowDown />
+            <small>min</small>
+            </div>
+            <h2>32 °C</h2>
+        </div>
+        <div className="card">
+            <div className="description__card-icon">
+            <FaArrowDown />
+            <small>min</small>
+            </div>
+            <h2>32 °C</h2>
         </div>
     </div>
   )
 };
 
 export default Description
+ 

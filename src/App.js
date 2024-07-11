7bda7f798af9d2695fda03827ce9e73708ad8adf
PRASHANTKUMAR_7 <prashantkumar74806@gmail.com>
2024-07-11 11:57:53 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+// importing image as ketword
+
 import hotbg from "./assets/hot.jpg";
 import coldbg from "./assets/cold.jpg";
 import hazebg from "./assets/haze.jpg";
 import rainbg from "./assets/rainy.jpeg";
+import other from "./assets/otherr.jpg";
+
 import Description from "./components/Description";
 import { useEffect, useState } from "react";
 import { getInfo } from "./weatherService";
 
 
 function App() {
+  const [city, setCity] = useState("Varanasi");
   const [weather, setWeather]= useState(null);
   const[units, setUnits]=useState("metric");
+  const [bg, setBg] = useState(other);
 
   useEffect(() => {
     const fetchWeatherData=async()=> {
-      const data= await getInfo("paris", units);
-     setWeather(data);
+      const data= await getInfo(city, units);
+      setWeather(data);
+
+    // dynamic background
+      const threshold = units === "metric" ? 20 : 60;
+      if(data.temp <=threshold) setBg(coldbg);
+      else setBg(hotbg);
+
     };
 
     fetchWeatherData();
-  },[units]);
+  },[units, city]);
 
 // function to handle units dynamically Alt+0176 for ° "degree"
 
@@ -32,17 +44,25 @@ const handleUnitsClick = (e) => {
 };
 // **************************
 
+// handling test field
+const enterKetPressed = (e) => {
+  if(e.keyCode === 13){
+    setCity(e.currentTarget.value);
+    e.currentTarget.blur();
+  }
+};
 
 
   return (
-    <div className="app" style={{backgroundImage: `url(${coldbg})`}}>
+    <div className="app" style={{backgroundImage: `url(${bg})`}}>
+
       <div className="overlay">
         {
           // now container will render when weather will be there because we put container div in weather 
         weather && (
           <div className="container">
           <div className="section section__inputs">
-            <input type="text" name="city" placeholder="Enter City Name..."/>            
+            <input onKeyDown={enterKetPressed} type="text" name="city" placeholder="Enter City Name..."/>            
             <button onClick={(e) => handleUnitsClick(e)}>°F</button>
           </div>
           <div className="section section__temperature">
@@ -52,14 +72,15 @@ const handleUnitsClick = (e) => {
               <h3>{weather.description}</h3>
             </div>
             <div className="temperature">
-              <h1>{`${weather.temp.toFixed()} °${units === "metric" ? "C" : "F"
-              }`}</h1>
-            </div>
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
           </div>
           {/* bottom description */}
           <Description weather={weather} units={units} />
         </div>
-        )}
+       )}
       </div>
     </div>
   );
